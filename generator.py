84963b7a41a4902997c553891415ea62e2cf4db2
Fabian Kneißl <fabian.kneissl@ifi.lmu.de>
2014-03-13 13:07:30 +0100

    exhibit3 table as timeline

--- generator.py
+++ generator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import postgresql
-import markup
-from markupsafe import escape
+import json
 import sys
 import datetime
+from icalendar import Event
 
 class Generator:
 	def __init__(self):
@@ -15,33 +15,44 @@ class Generator:
 		ps = self.db.prepare("""select o.startdate, o.enddate, summary, calendardata 
 				from oc_clndr_objects o 
 				join oc_clndr_calendars c on o.calendarid=c.id 
-				where userid='fabi' 
+				where userid='fabi' and objecttype='VEVENT'
 				order by startdate asc""")
 
-		self.xml = markup.page(mode='xml')
-		self.xml.data.open()
+		self.data = []
 		for startdate, enddate, summary, data in ps():
-			if startdate != None and len(summary) > 0:
+			if startdate != None and enddate != None and len(summary) > 0:
 				summary = self.myescape(summary)
-				if enddate - startdate <= datetime.timedelta(hours=24):
-					self.xml.event(summary, start=str(startdate), end=str(enddate), title=summary)
-				else:
-					self.xml.event(summary, start=str(startdate), end=str(enddate), isDuration="true", title=summary)
-		self.xml.data.close()
-		return self.xml
+				item = {
+					'type': 'Event',
+					'label': summary, 
+					'start': str(startdate),
+					'end': str(enddate)
+				}
+				if enddate - startdate > datetime.timedelta(hours=24):
+					item['isDuration'] = True
+				# parse ical event
+				icalCalendar = Event.from_ical(data)
+				for comp in icalCalendar.subcomponents:
+					if comp.name == 'VEVENT':
+						item['description'] = comp.get('description')
+						item['location'] = comp.get('location')
+						item['categories'] = comp.get('categories')
+						item['id'] = comp.get('uid')
+				self.data.append(item)
+		return self.data
 
 	def myescape(self, text):
-		return escape(text.replace('\\',''))
+		return text.replace('\\','')
 
 	def write(self, outfile):
 		with open(outfile, "w") as f:
-			f.write(str(self.xml))
+			f.write(json.dumps({'items': self.data}, indent=2))
 
 	def _openSql(self, sqlLocator):
 		self.db = postgresql.open(sqlLocator)
 
 if __name__ == "__main__":
-	outfile = "calendar.xml"
+	outfile = "calendar.js"
 	g = Generator()
 	g.read()
 	g.write(outfile)
